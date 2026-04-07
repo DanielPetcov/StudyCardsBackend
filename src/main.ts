0dfaa5ece6c0848c7e57e8dc4883b059708021cf
daniel petcov <danielpetcov.work@gmail.com>
2026-04-07 18:11:04 +0300

    added port log

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 import { setAppContext } from './app-context';
 
 async function bootstrap() {
+  const port = Number(process.env.PORT ?? 4000);
+  console.log('PORT ENV =', process.env.PORT);
+  console.log('LISTENING ON =', port);
+
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
   });
@@ -16,5 +20,7 @@ async function bootstrap() {
   });
   setAppContext(app);
   await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
+
+  console.log(`Server is running on 0.0.0.0:${port}`);
 }
 bootstrap();
